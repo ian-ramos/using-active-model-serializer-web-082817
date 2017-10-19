b7508263b2a1c1510edfa4fc75b5fc5fc4ad7a0c
ian-ramos <ramos.ianc@gmail.com>
2017-10-19 13:57:41 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,9 +9,8 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     respond_to do |format|
       format.html { render :show }
-      format.json { render json: @post.to_json(only: [:title, :description, :id],
-                              include: [author: { only: [:name]}]) }
-    end
+      format.json { render json: @post}
+    end    
   end
 
   def new
